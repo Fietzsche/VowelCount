8980557cc63bc234dc4fdd328ebc19fd6bc848ac
Jared <realjaredeveritt@gmail.com>
2018-09-01 08:18:46 -0500

    Fixed minor bugs

--- VowelCount.py
+++ VowelCount.py
@@ -10,14 +10,14 @@ vowelNo = lowerText.count('a') + lowerText.count('e') + lowerText.count('i') + l
 vowelNoY = vowelNo + lowerText.count('y')
 
 # Determines if "Vowel" or "Vowels" is used from whether or not there's only 1 vowel
-if vowelNo > 1:
+if vowelNo > 1 or vowelNo == 0:
     vowelText = " vowels"
-elif vowelNo <= 1:
+elif vowelNo == 1:
     vowelText = " vowel"
 
 # Adds extra text if y is in the string, as it's sometimes a vowel
-if 'y' not in text:
+if 'y' not in lowerText:
     print("The text you've entered has " + str(vowelNo) + vowelText + " in it.")
-elif 'y' in text:
+elif 'y' in lowerText:
     print("The text you've entered has " + str(vowelNo) + vowelText + " in it.")
     print("(" + str(vowelNoY) + " if you count Y)")
