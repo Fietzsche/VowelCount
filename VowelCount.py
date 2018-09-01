af218316aba28ec253d320cfb5dbe0006666300a
Jared <realjaredeveritt@gmail.com>
2018-08-31 20:59:08 -0500

    Removed case sensitivity when counting vowels

--- VowelCount.py
+++ VowelCount.py
@@ -1,11 +1,13 @@
 # Vowel Count program
 # Counts number of vowels in a given piece of text
 
-# Gets string from user
 text = input("Type something\n")
 
-vowelNo = text.count('a') + text.count('e') + text.count('i') + text.count('o') + text.count('u')
-vowelNoY = vowelNo + text.count('y')
+# Converts text to lowercase so vowel counting won't be case sensitive
+lowerText = text.lower()
+
+vowelNo = lowerText.count('a') + lowerText.count('e') + lowerText.count('i') + lowerText.count('o') + lowerText.count('u')
+vowelNoY = vowelNo + lowerText.count('y')
 
 # Determines if "Vowel" or "Vowels" is used from whether or not there's only 1 vowel
 if vowelNo > 1:
@@ -18,4 +20,4 @@ if 'y' not in text:
     print("The text you've entered has " + str(vowelNo) + vowelText + " in it.")
 elif 'y' in text:
     print("The text you've entered has " + str(vowelNo) + vowelText + " in it.")
-    print("(" + str(vowelNoY) + " if you count y)")
+    print("(" + str(vowelNoY) + " if you count Y)")
